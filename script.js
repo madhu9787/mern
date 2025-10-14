d8ffd07b6c5608653c2294550256a7fe0c63d134
madhu9787 <madhumitha805632@gmail.com>
2025-10-14 14:34:29 +0530

    final

--- script.js
+++ script.js
@@ -111,8 +111,26 @@ let name='mad';
 let age=19;
 console.log(` I am ${name} and my age is ${age}`);
 
+const mul=(a,b)=>{
+    return a*b;
+}
+const square=(n)=>{
+    return mul(n,n);
+}
+const print=(n)=>{
+    let s=square(n);
+    console.log(s);
+}
+print(4);
+//event loop
 
+console.log("first");
+Promise.resolve().then(console.log("From promises"))
+setTimeout(()=>{
+    console.log("Inside Timeout");
+},2000);
 
+console.log("last");
 
 
 
